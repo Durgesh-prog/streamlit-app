3d406cad850c8c95c7b2e42df2d246a0ddf96cb5
Dg <62983973+Durgesh-prog@users.noreply.github.com>
2023-04-09 23:39:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ number3 = st.number_input("Number 3")
 st.subheader('Result')
 if number1 > number2 and number1 > number3:
       st.write('Number 1 is the largest')
-else if  number2 > number1 and number2 > number3:
+elif  number2 > number1 and number2 > number3:
      st.write('Number 2 is the largest')
-else if  number3 > number1 and number3 > number2:
+elif  number3 > number1 and number3 > number2:
      st.write('Number 3 is the largest')
 
