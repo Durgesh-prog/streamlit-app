7b9dacb01ef9f7b1694c8791d840192d190c31a6
Dg <62983973+Durgesh-prog@users.noreply.github.com>
2023-04-09 23:34:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,22 +10,12 @@ This app find the largest number among three numbers
 
 st.header('Input Parameters')
 
-def user_input_features():
-    number1 = st.number_input("Number_1")
-    number2 = st.number_input("Number_2")
-    number3 = st.number_input("Number_3")
-   
-    data = {'Number_1': number1,
-           'Number_2': number2,
-            'Number_3': number3,
-            }
-    features = pd.DataFrame(data, index=[0])
-    return features
-
-df = user_input_features()
+number1 = st.number_input("Number_1")
+number2 = st.number_input("Number_2")
+number3 = st.number_input("Number_3")
 
 st.subheader('Prediction')
-if df['Number_1'] == 10:
+if number1 == 10:
     st.write('Approved')
 else:
     st.write('Declined')
